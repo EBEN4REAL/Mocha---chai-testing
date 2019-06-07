974daa761b2e20e8cb591ba8d2e2010865a544b1
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2019-06-07 09:19:47 -0700

    test

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 module.exports = {
 	sayHello: () => {
 		return "Hello";
+	},
+	addNumbers: (val1, val2) => {
+		return val1 + val2;
 	}
 }
--- test/appTest.js
+++ test/appTest.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 
 describe('App'  , () => {
 	it('sayHello should return hello' , () => {
@@ -8,5 +9,9 @@ describe('App'  , () => {
 	it('sayHello should return type string' , () => {
 		let result = sayHello();
 		assert.typeOf(result, 'string');
+	})
+	it('addNumbers should be above 5' , () => {
+		let result = addNumbers(5,5);
+		assert.isAbove(result, 5);
 	})
 })
